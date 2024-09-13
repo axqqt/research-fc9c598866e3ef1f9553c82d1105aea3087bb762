6ded592f88ecef4d4c9a51069ffed0ece3ecb608
Veloxal <127298642+axqqt@users.noreply.github.com>
2024-09-13 16:30:55 +0530

    qwtqwt

--- app/page.js
+++ app/page.js
@@ -51,8 +51,8 @@ export default function Home() {
 
   return (
     <div className="container mx-auto p-4">
+           <br />
       <h1 className="text-2xl font-bold mb-4">AliExpress Product Finder</h1>
-      <br />
       {/* <Link href={"/download"} style={{ margin: "40px" }}>
         Tiktok Scraping
       </Link> */}
